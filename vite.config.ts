8442858e77b5400d7d3368333132eedb0a7f60d8
Abdalla Taha <53947286+abdallataha@users.noreply.github.com>
2026-08-27 20:54:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/game-snake-ladders/', // 👈 Add this line here
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
